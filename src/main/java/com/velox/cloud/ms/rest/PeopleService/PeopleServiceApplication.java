53cdd114e6c5b5451dee4f396a5afb04c1c8f897
Claudiu Spataru <claudiu.spataru@gmail.com>
2018-09-20 22:40:25 +0300

    First commit

---
+++ src/main/java/com/velox/cloud/ms/rest/PeopleService/PeopleServiceApplication.java
@@ -0,0 +1,12 @@
+package com.velox.cloud.ms.rest.PeopleService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleServiceApplication.class, args);
+	}
+}
